3e160d40c149544d42f7d1374abbdc293c892c82
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-02-05 15:37:20 +0300

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Владимир Рябов on 05.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
